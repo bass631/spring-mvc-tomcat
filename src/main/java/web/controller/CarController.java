386bc8f14519a8fa5d6f84016cf12ad66003f6e9
bass631 <dima.bass@mail.ru>
2022-02-02 11:59:11 +0300

    2.2.2 v.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,32 +1,30 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.dao.CarDao;
 import web.model.Car;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
 
+    @Autowired
+    private CarDao carDao;
+
     @GetMapping(value = "/cars")
     public String cars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
 
-        final List<Car> cars = new ArrayList<>();
-        cars.add((new Car("Model_1", 1, "Color_1")));
-        cars.add((new Car("Model_2", 2, "Color_2")));
-        cars.add((new Car("Model_3", 3, "Color_3")));
-        cars.add((new Car("Model_4", 4, "Color_4")));
-        cars.add((new Car("Model_5", 5, "Color_5")));
-
         if (count == null) {
-            model.addAttribute("resultCars", cars);
+            model.addAttribute("resultCars", carDao.indexCars());
         } else {
-            final List<Car> qtyCars = cars.stream().limit(count).collect(Collectors.toList());
+            final List<Car> qtyCars = carDao.indexCars().stream().limit(count).collect(Collectors.toList());
             model.addAttribute("resultCars", qtyCars);
         }
         return "car";
---
+++ src/main/java/web/dao/CarDao.java
@@ -0,0 +1,25 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarDao {
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+        cars.add((new Car("Model_1", 1, "Color_1")));
+        cars.add((new Car("Model_2", 2, "Color_2")));
+        cars.add((new Car("Model_3", 3, "Color_3")));
+        cars.add((new Car("Model_4", 4, "Color_4")));
+        cars.add((new Car("Model_5", 5, "Color_5")));
+    }
+
+    public List<Car> indexCars() {
+        return cars;
+    }
+}
